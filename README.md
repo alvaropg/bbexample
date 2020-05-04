a36542a8c8a9f43ba3e8daf070c8641844e22166
Álvaro Peña <alvaropg@gmail.com>
2020-05-04 19:29:13 +0200

    [glib] Added GLib dependency

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 bb-example
 ==========
 
-Example Yocto/OpenEmbedded autotooled recipe, building an executable with a dependency on a shared library
+Example Yocto/OpenEmbedded autotooled recipe, building an executable with a dependency on a shared library.
 
+Just added to original bb-example a dependency to glib & gio, just to check a GApplication
--- bbexample.c
+++ bbexample.c
@@ -6,7 +6,15 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <gio/gio.h>
+
 #include "bbexample.h"
+
+void app_activate (GApplication *application,
+                   gpointer      user_data)
+{
+  g_print("Application is activate\n");
+}
 
 int main(int argc, char *argv[])
 {
@@ -14,7 +22,10 @@ int main(int argc, char *argv[])
 
   LibHelloWorld();
 
-  return 0;
+  GApplication *app;
+  app = g_application_new("com.github.alvaropg.bbexample", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect(app, "activate", G_CALLBACK(app_activate), NULL);
+  return g_application_run(app, argc, argv);
 }
 
 
